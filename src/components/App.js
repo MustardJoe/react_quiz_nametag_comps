016eb8654e8e8cbd60457adfa225cf932c9706cf
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-19 14:57:47 -0700

    doesn't work

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import NameTag from './NameTag/NameTag';
 export default function App() {
   return (
     <>
-      <h1>Hello World, enter 1. your name, and 2. a greeting in the fields below!</h1>
+      <h1>Hello World, enter 1. your name!</h1>
       <NameTag />
     </>
   ); 
