b3597962eba494abbb5050e3173edd066b41fcc8
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-19 22:44:59 -0700

    i feel like i have some good bits of code but obviously not enough...

--- src/components/NameTag/NameTag.js
+++ src/components/NameTag/NameTag.js
@@ -4,13 +4,31 @@ import ColorSelector from '../ColorSelector/ColorSelector';
 import Shape from '../Shape/Shape';
 
 export default class NameTag extends Component {
+  state = {
+    color: '',
+    backgroundColor: '',
+  }
+
+  handleColorChange = ({ target }) => {
+    this.setState({ color: target.value });
+  }
+
+  handleBackgroundColorChange = ({ target }) => {
+    this.setState({ backgroundColor: target.value });
+  }
 
   render() {
+    const { backgroundColor, color } = this.state;
+
     return (
       <>
         <p>name:</p>
         <TextSelector />
-        <ColorSelector />
+        <ColorSelector 
+          color={color}
+          handleColorChange={this.handleColorChange}
+          backgroundColor={backgroundColor}
+          handleBackgroundColorChange={this.handleBackgroundColorChange}/>
         <Shape />
       </>
     ); 
